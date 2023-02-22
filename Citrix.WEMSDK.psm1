deadb02cf64eb2d6685597f516077661efe171c0
Fabian Ascheberg <88002233+Stachelritter@users.noreply.github.com>
2023-02-22 14:22:28 +0100

    Remove spaces at the end of the line

--- Citrix.WEMSDK.psm1
+++ Citrix.WEMSDK.psm1
@@ -275,7 +275,7 @@ function New-ChangesLogEntry {
 
     if (-not $UserId) { $UserId = "[Citrix.WEMSDK] $($env:USERDOMAIN)\$($env:USERNAME)"}
 
-    $SQLQuery = "INSERT INTO VUEMChangesLog (IdSite,IdElement,UserId,ChangeType,ObjectName,ObjectType,ChangeDate,NewValue,ChangeDescription,Reserved01) VALUES ($($IdSite),$($IdElement),'$($UserId)','$($ChangeType)','$($ObjectName)','$($ObjectType)',GETDATE(),'$($NewValue)',"    
+    $SQLQuery = "INSERT INTO VUEMChangesLog (IdSite,IdElement,UserId,ChangeType,ObjectName,ObjectType,ChangeDate,NewValue,ChangeDescription,Reserved01) VALUES ($($IdSite),$($IdElement),'$($UserId)','$($ChangeType)','$($ObjectName)','$($ObjectType)',GETDATE(),'$($NewValue)',"
     if ($ChangeDescription) { 
         $SQLQuery += "'$($ChangeDescription)',"
     } else { 
